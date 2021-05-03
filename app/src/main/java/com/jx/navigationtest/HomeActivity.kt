68c28301d858468317fa1f2b2ec27e3b98f463c1
Jeff Soares <jeffersonvsoares@hotmail.com>
2021-05-02 22:22:28 -0300

    Change bottom navigation to work with fragments and linked with navController

--- app/src/main/java/com/jx/navigationtest/HomeActivity.kt
+++ app/src/main/java/com/jx/navigationtest/HomeActivity.kt
@@ -1,46 +1,29 @@
 package com.jx.navigationtest
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
 
-class HomeActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
-
-    private lateinit var navMenu: BottomNavigationView
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        findViewById<Button>(R.id.start_activity2).setOnClickListener {
-            val mIntent = Intent(this, SecondActivity::class.java)
-            startActivity(mIntent)
-            finish()
-        }
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.findNavController()
 
-        navMenu = findViewById(R.id.nav_bottom_menu)
-        navMenu.selectedItemId = R.id.home
-        navMenu.setOnNavigationItemSelectedListener(this)
+        val appBarConf = AppBarConfiguration(setOf(R.id.first_fragment, R.id.second_fragment, R.id.third_fragment))
+        setupActionBarWithNavController(navController, appBarConf)
 
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.home -> return true
-            R.id.second -> startActivity(Intent(this, SecondActivity::class.java))
-            R.id.third -> startActivity(Intent(this, ThirdActivity::class.java))
-        }
-        finish()
-        return true
-    }
-
-    override fun onPause() {
-        super.onPause()
-        overridePendingTransition(0, 0)
+        bottomNavigation.setupWithNavController(navController)
     }
 
 }
